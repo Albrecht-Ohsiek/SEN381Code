09302f757b4b7f881f5df1e79cf5cacaa6ccc3b4
Albrecht Ohsiek <84184217+Albrecht-Ohsiek@users.noreply.github.com>
2023-11-06 09:51:21 +0200

    Login data access fix (#17)
    * Fixed Login Controller
    
    * cleanup
    
    ---------
    
    Co-authored-by: Albrecht Ohsiek <albrecht.ohsiek@eps.ch>

--- CallCenter/Database/DBconnect.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CallCenter.Database
-{
-    public class DBconnect
-    {
-        public string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;TrustServerCertificate=true;AttachDbFilename=|DataDirectory|\\Database\\SEN381_DATABASE.mdf;Initial Catalog=SEN381_DATABASE; Integrated Security=True;Connect Timeout=30";
-    }
-}
