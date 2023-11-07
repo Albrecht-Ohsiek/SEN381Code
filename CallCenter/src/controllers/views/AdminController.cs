55c890c25d4f466d0af69e6442935543b7eec51e
Ruan Ludick <ruludick2@gmail.>
2023-11-07 18:28:11 +0200

    DetailsAndEdits

--- CallCenter/src/controllers/views/AdminController.cs
+++ CallCenter/src/controllers/views/AdminController.cs
@@ -1,6 +1,7 @@
 using CallCenter.Controllers;
 using CallCenter.Repository;
 using CallCenter.Types;
+using CallCenter.Models;
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,8 +29,8 @@ namespace CallCenter.src.controllers
         }
 
         public async Task<IActionResult> AdminRequestLogs()
-        {
-            return View(await _technicianRepository.GetAllTechnicians());
+        {    
+            return View(await _requestLogRepository.GetAllRequestLogs());
         }
 
         // GET: Employee/Details/?
@@ -48,5 +49,47 @@ namespace CallCenter.src.controllers
 
             return View(employee);
         }
+
+        public async Task<IActionResult> EmployeeEdit(UpdateEmployeeRequest emp)
+        {
+            if (emp == null)
+            {
+                return NotFound();
+            }
+
+            // Assume emp.department is of type Department?
+            CallCenter.Types.Department nonNullableDepartment;
+
+            // Check if emp.department has a value, and if not, assign a default value
+            if (emp.department.HasValue)
+            {
+                // Use the Value property to access the non-nullable value
+                nonNullableDepartment = emp.department.Value;
+            }
+            else
+            {
+                // Assign a default value (you can use a different default value)
+                nonNullableDepartment = CallCenter.Types.Department.Undefined;
+            }
+
+            // Create an Employee object and set its properties
+            Employee employee = new Employee
+            {
+                // Assign other properties
+                employeeId = emp.employeeId,
+                employeeName = emp.employeeName,
+                // Assign the non-nullable department
+                department = nonNullableDepartment,
+                emailAddress = emp.emailAddress,
+                phoneNumber = emp.phoneNumber
+            };
+
+            // Update the employee
+            await _employeeRepository.UpdateEmployee(employee);
+
+            // Redirect to the EmployeeDetails page
+            return RedirectToAction("EmployeeDetails", new { id = emp.employeeId });
+        }
+
     }
 }
--- CallCenter/src/controllers/views/ClientMaintenanceController.cs
+++ CallCenter/src/controllers/views/ClientMaintenanceController.cs
@@ -6,9 +6,37 @@ namespace CallCenter.Controllers
 {
     public class ClientMaintenanceController : Controller 
     {
-        public IActionResult ClientMaintenance()
+        private readonly IClientRepository _clientRepository;
+        public ClientMaintenanceController(IClientRepository clientRepository)
         {
-            return View();
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<IActionResult> ClientMaintenance()
+        {
+            return View(await _clientRepository.GetAllClients());
+        }
+
+        // GET: Employee/Details/?
+        public async Task<IActionResult> ClientDetails(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _clientRepository.GetClientById(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        public async Task<IActionResult> ClientEdit()
+        {
+            return View(await _clientRepository.GetAllClients());
         }
     }
 }
